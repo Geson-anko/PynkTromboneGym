fdec318b9051f45131ab92c0dfd1e86cb955018c
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-09-01 13:53:48 +0900

    Update test_spectrogram.py
    ADD test_stft

--- tests/test_spectrogram.py
+++ tests/test_spectrogram.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from pynktrombonegym import spectrogram as spct
 
 
@@ -7,3 +9,10 @@ def test_calc_rfft_channel_num():
     assert f(256) == 129
     assert f(400) == 201
     assert f(501) == 251
+
+
+def test_stft():
+    f = spct.stft
+    wave = np.sin(np.linspace(0, 2 * np.pi, 1024))
+    out = f(wave, 1024, 256)
+    assert out.dtype == np.complex128
