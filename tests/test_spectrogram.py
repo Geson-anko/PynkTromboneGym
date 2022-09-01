6fbf6b6c5f6581538d6f97bc0685c6d251950d18
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-09-01 14:00:03 +0900

    Update test_spectrogram.py
    ADD test_calc_target_sound_spectrogram_length and update test_stft

--- tests/test_spectrogram.py
+++ tests/test_spectrogram.py
@@ -11,8 +11,18 @@ def test_calc_rfft_channel_num():
     assert f(501) == 251
 
 
+def test_calc_target_sound_spectrogram_length():
+    f = spct.calc_target_sound_spectrogram_length
+    assert f(512, 1024, 256) == 3
+    assert f(1024, 1024, 256) == 5
+    assert f(1024, 512, 128) == 9
+
+
 def test_stft():
     f = spct.stft
+    cf = spct.calc_rfft_channel_num
+    lf = spct.calc_target_sound_spectrogram_length
     wave = np.sin(np.linspace(0, 2 * np.pi, 1024))
     out = f(wave, 1024, 256)
     assert out.dtype == np.complex128
+    assert out.shape == (lf(len(wave), 1024, 256), cf(1024))
