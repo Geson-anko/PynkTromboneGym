5a1eb3698229a05dc94045f961551a538ec8376e
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-09-18 20:51:49 +0900

    Update test_action_by_acceleration.py
    ADD test_convert_space_to_acceleration

--- tests/wrappers/test_action_by_acceleration.py
+++ tests/wrappers/test_action_by_acceleration.py
@@ -1,7 +1,11 @@
+import math
+
+from gym import spaces
+
 from pynktrombonegym.env import PynkTrombone
 from pynktrombonegym.wrappers.action_by_acceleration import ActionByAcceleration
 
-from ..test_env import target_sound_files
+from ..test_env import assert_space, target_sound_files
 
 
 def test__init__():
@@ -19,3 +23,22 @@ def test__init__():
     wrapped = ActionByAcceleration(base, action_scaler, initial_pos=init_act)
     assert wrapped.action_scaler == action_scaler
     assert wrapped.initial_pos is init_act
+
+
+def test_convert_space_to_acceleration():
+    cls = ActionByAcceleration
+
+    b = spaces.Box(-1, 1)
+    f = cls.convert_space_to_acceleration
+    assert_space(f(b), b)
+    assert_space(f(spaces.Box(0, 1)), spaces.Box(-0.5, 0.5))
+    assert_space(f(spaces.Box(-10, 2, dtype=int)), spaces.Box(-6, 6, dtype=int))
+    assert_space(f(spaces.Box(0.0, 5.0, shape=(10, 2))), spaces.Box(-2.5, 2.5, shape=(10, 2)))
+    assert_space(f(spaces.Box(0, math.inf)), spaces.Box(-math.inf, math.inf))
+    assert_space(f(spaces.Box(-math.inf, 0)), spaces.Box(-math.inf, math.inf))
+    assert_space(f(spaces.Box(-math.inf, math.inf)), spaces.Box(-math.inf, math.inf))
+    assert_space(f(spaces.Box(1, 1)), spaces.Box(0, 0))
+
+    # Gym API Error
+    # assert_space(f(spaces.Box(math.inf, math.inf)), spaces.Box(0.0, 0.0))
+    # Box(math.inf, math.inf) -> Box(-inf, inf, (1,), float32) !!!!!!
