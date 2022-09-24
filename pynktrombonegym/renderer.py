7208d61dcb5193d2551caa7d93075a465d36b6f7
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2022-09-24 13:22:51 +0900

    Update renderer.py
    UPDATE Renderer class so that it renders `Voc` class directly.

--- pynktrombonegym/renderer.py
+++ pynktrombonegym/renderer.py
@@ -1,46 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
-from .env import PynkTrombone
+from pynktrombone.voc import Voc
 
 
 class Renderer:
     """Render process of PynkTrombone environment.
 
     Plotting:
-    - current_step
     - current_tract_diameters
     - nose_diameters
     - current voc frequency
     - current voc tenseness
     """
 
-    def __init__(self, env: PynkTrombone, figsize: tuple[float, float] = (6.4, 4.8)) -> None:
+    def __init__(self, voc: Voc, figsize: tuple[float, float] = (6.4, 4.8)) -> None:
         """Construct Renderer and create intial figure and etc...
         Args:
-            env (PynkTrombone): PynkTrombone vocal tract environment model.
+            voc (Voc): vocal tract model.
             figsize (tuple[float, float]): Figure size of rendered image. [Inch]
         """
 
-        self.env = env
+        self.voc = voc
         self.figsize = figsize
         self.create_initial_components()
 
     def make_infomation_text(self) -> str:
         """Make infomation text displaying on the figure.
         Infomations are:
-            - current_step
             - frequency
             - tenseness
 
         Returns:
             info_text (str): Infomation text value.
         """
-        info = (
-            f"current step: {self.env.current_step}\n"
-            f"frequency: {float(self.env.voc.frequency): .2f}\n"
-            f"tenseness: {float(self.env.voc.tenseness): .2f}\n"
-        )
+        info = f"frequency: {float(self.voc.frequency): .2f}\n" f"tenseness: {float(self.voc.tenseness): .2f}\n"
 
         return info
 
@@ -49,14 +42,14 @@ class Renderer:
         Componentes are Figure, axes, lines, and etc...
         """
 
-        nose_diameters = self.env.voc.nose_diameters
-        currect_tract_diameters = self.env.voc.current_tract_diameters
+        nose_diameters = self.voc.nose_diameters
+        currect_tract_diameters = self.voc.current_tract_diameters
 
         self.figure = plt.figure(figsize=self.figsize)
         self.axes = self.figure.add_subplot(1, 1, 1)
 
-        self.indices = list(range(self.env.voc.tract_size))
-        self.nose_indices = self.indices[-self.env.voc.nose_size :]
+        self.indices = list(range(self.voc.tract_size))
+        self.nose_indices = self.indices[-self.voc.nose_size :]
 
         self.axes.set_ylim(0.0, 5.0)
         self.nose_diameters_line = self.axes.plot(self.nose_indices, nose_diameters, label="nose diameters")[0]
@@ -73,8 +66,8 @@ class Renderer:
 
     def update_values(self) -> None:
         """Update values ploted on the figure."""
-        nose_diameters = self.env.voc.nose_diameters
-        currect_tract_diameters = self.env.voc.current_tract_diameters
+        nose_diameters = self.voc.nose_diameters
+        currect_tract_diameters = self.voc.current_tract_diameters
 
         info_text = self.make_infomation_text()
         self.tract_diameters_line.set_data(self.indices, currect_tract_diameters)
